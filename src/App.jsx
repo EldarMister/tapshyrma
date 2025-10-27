2745190e9a593ff603fc69a3ac1b285a2e1b4bba
EldarMister <marselanarbekovv@gmail.com>
2025-10-27 17:28:11 +0600

    Финальные правки для страницы корзины

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Cart from './components/Cart';
+import Cart from './components/Cart.jsx';
 import './components/Cart.css'; // Подключаем наши новые стили
 
 function App() {
